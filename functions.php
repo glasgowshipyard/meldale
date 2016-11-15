ea9ade7c95bae9c29ff3ad15d852e87732738464
Craig Zielinski <z@killinski.com>
2016-11-15 13:37:13 -0800

    Theme Styling
    Began styling theme.

--- functions.php
+++ functions.php
@@ -107,6 +107,9 @@ function meldale_scripts() {
 	wp_enqueue_script( 'meldale-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'meldale-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	
+	wp_enqueue_style('meldale-google-fonts','https://fonts.googleapis.com/css?family=PT+Mono', true);
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
